5b4db1dfaae7d234821391955280e4f9a838ad38
Elkinrodriguez03 <elkinrodriguez03@gmail.com>
2023-08-04 22:20:05 -0500

    Working with the CRUD

---
+++ routes/usersRouter.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('There is not params!');
+  }
+});
+
+router.get('/test', (req, res) => {
+  res.send('This is a fucking test');
+})
+
+module.exports = router;
