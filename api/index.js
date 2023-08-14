12dfc7d5020be5f4a1718952a628ff8ed1fa3c02
Elkinrodriguez03 <elkinrodriguez03@gmail.com>
2023-08-14 15:03:11 -0500

    Bug fixed

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whiteList = ['http://127.0.0.1:3000', "https://e-commerce-git-main-elkinrodriguez03.vercel.app"];
+const whiteList = ['http://127.0.0.1:3000', "https://e-commerce-git-main-elkinrodriguez03.vercel.app", "https://e-commerce-phi-seven.vercel.app/"];
 const options = {
   origin: (origin, callback) => {
     if (whiteList.includes(origin)) {
